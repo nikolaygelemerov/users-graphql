69f7d98d90efefb3d343e906bee7f2592068784b
nikolaygelemerov <nikolaygelemerov@gmail.com>
2021-07-05 19:10:13 +0300

    update npm dependencies

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
 const PORT = 4000;
 
-app.use('/graphql', expressGraphQL({ schema, graphiql: true }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(PORT, () => {
   console.log(`app listening on port: ${PORT}`);
